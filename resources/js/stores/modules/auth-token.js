36bed69d0b89e279a7aadfc7164c52ab06116653
tanveershuvo <tanveershuvos@gmail.com>
2021-03-12 19:22:37 +0600

    Solved login problem

--- resources/js/stores/modules/auth-token.js
+++ resources/js/stores/modules/auth-token.js
@@ -2,7 +2,7 @@ import App from './../../app'
 class AuthToken{
     isValid(token){
         const payload = this.payload(token);
-        return payload.iss === (App.cons_url + "auth/login");
+        return payload.iss.split('//')[1] === (App.cons_url + "auth/login").split('//')[1]
     }
     payload(token){
         const payload = token.split('.')[1];
